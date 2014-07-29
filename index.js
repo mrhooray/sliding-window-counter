56398b37dda925dad267b382fa871e5de8e25776
Rui Hu <code@mrhooray.com>
2014-07-29 01:27:22 -0700

    refactor

--- index.js
+++ index.js
@@ -1,60 +1,64 @@
 'use strict';
 
-var Window = function (keys) {
+function Bucket(keys) {
   var self = this;
 
   keys.forEach(function(key) {
     self[key] = 0;
   });
-};
+}
 
-var SlidingWindowCounter = function (opts) {
+function SlidingWindowCounter(opts) {
+  if (!(this instanceof SlidingWindowCounter)) {
+    return new SlidingWindowCounter(opts);
+  }
+
   var self = this;
 
   opts = opts || {};
-  opts.timeInMs = opts.timeInMs || 10 * 1000;
-  opts.numWindows = opts.numWindows || 10;
+  opts.windowInMs = opts.windowInMs || 10 * 1000;
+  opts.numBuckets = opts.numBuckets || 10;
   opts.keys = opts.keys || ['success', 'failure', 'timeout'];
 
-  if (opts.timeInMs % opts.numWindows !== 0) {
-    throw new Error('timeInMs % numWindows should equal to 0');
-  } else if (opts.timeInMs < 0 || opts.numWindows < 0 || !Array.isArray(opts.keys)) {
+  if (opts.windowInMs % opts.numBuckets !== 0) {
+    throw new Error('windowInMs % numBuckets should equal to 0');
+  } else if (opts.windowInMs < 0 || opts.numBuckets < 0 || !Array.isArray(opts.keys)) {
     throw new Error('invalid options');
   } else if (opts.keys.indexOf('stats') > 0) {
     throw new Error('stats is a reserved key to retrieve rolling stats');
   }
 
-  self.numWindows = opts.numWindows;
-  self.interval = opts.timeInMs / opts.numWindows;
+  self.numBuckets = opts.numBuckets;
+  self.interval = opts.windowInMs / opts.numBuckets;
   self.keys = opts.keys;
   self.currIndex = 0;
-  self.windows = [];
+  self.buckets = [];
 
-  while (opts.numWindows > 0) {
-    self.windows.push(new Window(self.keys));
-    opts.numWindows--;
+  while (opts.numBuckets > 0) {
+    self.buckets.push(new Bucket(self.keys));
+    opts.numBuckets--;
   }
 
   setInterval(function () {
-    self.currIndex = (self.currIndex + 1) % self.numWindows;
-    self.windows[self.currIndex] = new Window(self.keys);
+    self.currIndex = (self.currIndex + 1) % self.numBuckets;
+    self.buckets[self.currIndex] = new Bucket(self.keys);
   }, self.interval);
 
   self.keys.forEach(function (key) {
     self[key] = function (i) {
-      self.windows[self.currIndex][key] += i || 1;
+      self.buckets[self.currIndex][key] += i || 1;
     };
   });
 
   self.stats = function () {
-    var result = new Window(self.keys);
-    self.windows.forEach(function (window) {
+    var result = new Bucket(self.keys);
+    self.buckets.forEach(function (bucket) {
       self.keys.forEach(function (key) {
-        result[key] += window[key];
+        result[key] += bucket[key];
       });
     });
     return result;
   };
-};
+}
 
 module.exports = SlidingWindowCounter;
